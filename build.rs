5406a828a3ec9ea8b6662c1eb12aa0297aa3ce33
Christian Brendlin <christian@brenlinonline.de>
2024-02-26 21:59:03 +0100

    v0.1.0

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Ok(value) = std::env::var("API_KEY_EX") {
+    if let Ok(value) = std::env::var("API_KEY") {
         println!("cargo:rustc-env=API_KEY_EX={}", value);
     }
 }
