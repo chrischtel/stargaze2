d430de4c89be97aa3c687016dc09b3ccc7e7bff3
Christian Brendlin <christian@brenlinonline.de>
2024-02-26 21:42:51 +0100

    Cargo-Dist CI

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Ok(value) = std::env::var("API_KEY") {
+    if let Ok(value) = std::env::var("API_KEY_EX") {
         println!("cargo:rustc-env=API_KEY_EX={}", value);
     }
 }
